fefa35f38e554409f112ec8bb2158566832584b2
elPeiretti <tomaspeiretti@gmail.com>
2022-12-21 18:40:06 -0300

    cosas para el backend2

---
+++ Subnet_Publica/backend2_init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## instalar y correr servidor apache
+sudo apt-get update
+sudo apt install apache2
+systemctl start apache2
+
+## cambiar el html
+rm /var/www/html/index.html
+cp ./backend2_index.html /var/www/html/index.html
+
+## reiniciar apache2
+systemctl restart apache2
