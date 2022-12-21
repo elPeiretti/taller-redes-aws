3f310a808efe9252c145c343167b772a7dac45e7
elPeiretti <tomaspeiretti@gmail.com>
2022-12-21 18:37:54 -0300

    un cambio

--- Subnet_Publica/backend1_init.sh
+++ Subnet_Publica/backend1_init.sh
@@ -5,5 +5,9 @@ sudo apt-get update
 sudo apt install apache2
 systemctl start apache2
 
+## cambiar el html
 rm /var/www/html/index.html
 cp ./backend1_index.html /var/www/html/index.html
+
+## reiniciar apache2
+systemctl restart apache2
