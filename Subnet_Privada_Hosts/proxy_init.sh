1050d5319dd21dc90694aaa3eb9deb00eec331a2
elPeiretti <tomaspeiretti@gmail.com>
2022-12-30 13:32:28 -0300

    configuracion para VPN en el proxy

--- Subnet_Privada_Hosts/proxy_init.sh
+++ Subnet_Privada_Hosts/proxy_init.sh
@@ -4,10 +4,31 @@
 sudo apt-get update
 sudo apt-get install squid
 
-## navegar al directorio y cambiar el archivo de configuracion
+## cambiar el archivo de configuracion
 sudo rm /etc/squid/squid.conf
 sudo cp ./squid.conf /etc/squid/squid.conf
 sudo cp ./dominios.squid /etc/squid/
 
 ## reiniciar squid
 sudo systemctl restart squid
+
+## instalar wireguard
+sudo apt install wireguard
+
+sudo su
+## generar claves
+cd /etc/wireguard
+#crear clave privada
+wg genkey > tp_private_key
+# crear clave publica
+wg pubkey < tp_private_key > tp_public_key
+
+# crear archivo de configuracion
+vi tr0.conf
+chmod 600 tp_private_key tp_public_key tr0.conf
+
+## levantar wireguard
+systemctl enable --now wg-quick@tr0
+
+## habilitar el enrutamiento
+echo "1" > /proc/sys/net/ipv4/ip_forward
