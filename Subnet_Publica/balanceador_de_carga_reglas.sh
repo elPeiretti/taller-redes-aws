12c8d85c29d80cd67a5c29af3667ff2c6170d193
ChortJulio <chortjulio@gmail.com>
2023-01-10 12:20:24 -0300

    balanceador de carga

--- Subnet_Publica/balanceador_de_carga_reglas.sh
+++ Subnet_Publica/balanceador_de_carga_reglas.sh
@@ -6,8 +6,28 @@ iptables -F OUTPUT
 iptables -F INPUT
 iptables -F FORWARD
 
+# Reglas para poder conectarse por ssh si se necesita
+
+iptables -A INPUT -i eth0 -s 0.0.0.0/0 -d 54.237.49.56 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -s 54.237.49.56 -d 0.0.0.0/0 -p tcp --sport 22 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+
 # Reglas por defecto
 
 iptables -P OUTPUT DROP
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
+
+# Reglas para permitir el trafico http y https desde afuera
+
+iptables -A INPUT -i eth0 -s 0.0.0.0/0 -d 54.237.49.56 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -s 54.237.49.56 -d 0.0.0.0/0 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -s 0.0.0.0/0 -d 54.237.49.56 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -s 54.237.49.56 -d 0.0.0.0/0 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+
+# Reglas para permitir el trafico desde el proxy
+iptables -A INPUT -i eth0 -s 10.0.0.26 -d 10.0.0.12 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -s 10.0.0.12 -d 10.0.0.26 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -s 10.0.0.26 -d 10.0.0.12 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -s 10.0.0.12 -d 10.0.0.26 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+
+
