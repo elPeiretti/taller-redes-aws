e3f0d9d0b37ead92a592c4130df324f5cabca85a
ChortJulio <chortjulio@gmail.com>
2023-02-08 17:38:48 -0300

    Servidor privado con VPN

--- Subnet_Privada_Servidores/servidor_privado_init.sh
+++ Subnet_Privada_Servidores/servidor_privado_init.sh
@@ -11,3 +11,24 @@ cp ./servidor_privado_index.html /var/www/html/index.html
 
 ## reiniciar apache2
 systemctl restart apache2
+
+## instalar wireguard
+sudo apt install wireguard
+
+sudo su
+## generar claves
+cd /etc/wireguard
+#crear clave privada
+wg genkey > tp_private_key
+# crear clave publica
+wg pubkey < tp_private_key > tp_public_key
+
+# crear archivo de configuracion
+vi tr0.conf
+chmod 600 tp_private_key tp_public_key tr0.conf
+
+## levantar wireguard
+systemctl enable --now wg-quick@tr0
+
+## habilitar el enrutamiento
+echo "1" > /proc/sys/net/ipv4/ip_forward
