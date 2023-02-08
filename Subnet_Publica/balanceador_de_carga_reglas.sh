1621e0efc18f553f4669077bd7dc64d1cb201c64
ChortJulio <chortjulio@gmail.com>
2023-02-08 12:29:11 -0300

    cambio ip privada

--- Subnet_Publica/balanceador_de_carga_reglas.sh
+++ Subnet_Publica/balanceador_de_carga_reglas.sh
@@ -20,26 +20,26 @@ iptables -P FORWARD DROP
 
 # Reglas para permitir el trafico http y https desde afuera
 
-iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 0.0.0.0/0 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 0.0.0.0/0 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 0.0.0.0/0 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 0.0.0.0/0 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
 
 # Reglas para permitir el trafico desde/hasta el proxy
-iptables -A INPUT -s 10.0.0.26/32 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 10.0.0.26/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -s 10.0.0.26/32 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 10.0.0.26/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.26/32 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 10.0.0.26/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.26/32 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 10.0.0.26/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
 
 # Reglas para permitir desde/hasta el Backend1
-iptables -A INPUT -s 10.0.0.4/32 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 10.0.0.4/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -s 10.0.0.4/32 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 10.0.0.4/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.4/32 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 10.0.0.4/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.4/32 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 10.0.0.4/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
 
 
 # Reglas para permitir desde/hasta el Backend2
-iptables -A INPUT -s 10.0.0.13/32 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 10.0.0.13/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -s 10.0.0.13/32 -d 10.0.0.14/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.14/32 -d 10.0.0.13/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.13/32 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 10.0.0.13/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.13/32 -d 10.0.0.12/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.12/32 -d 10.0.0.13/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
