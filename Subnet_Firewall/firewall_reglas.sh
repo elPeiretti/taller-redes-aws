dcddefc5a3fdca0adbf890a6f8c9e69fbee0fbec
elPeiretti <tomaspeiretti@gmail.com>
2023-02-12 17:02:01 -0300

    ahora si se rutea lo del vpn

--- Subnet_Firewall/firewall_reglas.sh
+++ Subnet_Firewall/firewall_reglas.sh
@@ -1,52 +1,48 @@
 #!/bin/bash
 
 # Borrar todas las reglas
-
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
 iptables -F OUTPUT
 iptables -F INPUT
 iptables -F FORWARD
+iptables -t nat -F PREROUTING
+iptables -t nat -F POSTROUTING
 
-# Reglas para poder conectarse por ssh si se necesita
+# Reglas para poder conectarse por ssh si se necesita al firewall
+iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.104/32 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.104/32 -d 0.0.0.0/0 -p tcp --sport 22 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
 
-iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.100/32 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.100/32 -d 0.0.0.0/0 -p tcp --sport 22 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-
-# Reglas para ssh para conectarse al resto de las instancias (Proxy por ejemplo)
+# Reglas para ssh para conectarse al resto de las instancias desde el firewall (Proxy por ejemplo)
 iptables -A INPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 
-# Reglas por defecto
+#### Reglas de NAT
+# Reglas para el servidor web público
+iptables -t nat -A PREROUTING -p tcp --dport 80 -d 10.0.0.104 -i eth0 -j DNAT --to-destination 10.0.0.12
+iptables -t nat -A POSTROUTING -p tcp --sport 80 -s 10.0.0.12 -o eth0 -j SNAT --to-source 10.0.0.104
+# Reglas para el proxy
+iptables -t nat -A POSTROUTING -p tcp --dport 80 -s 10.0.0.26 -o eth0 -j SNAT --to-source 10.0.0.104
+iptables -t nat -A POSTROUTING -p tcp --dport 443 -s 10.0.0.26 -o eth0 -j SNAT --to-source 10.0.0.104
+# Reglas para el servidor privado
+iptables -t nat -A PREROUTING -p udp --dport 51820  -d 10.0.0.104 -i eth0 -j DNAT --to-destination 10.0.0.39
+iptables -t nat -A POSTROUTING -p udp --sport 51820 -s 10.0.0.39 -o eth0 -j SNAT --to-source 10.0.0.104
 
+#### Reglas de FORWARD
+# Reglas para el servidor web público (NAT y FORWARD)
+iptables -A FORWARD -p tcp -s 0.0.0.0/0 -d 10.0.0.12/32 --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT 
+iptables -A FORWARD -p tcp -s 10.0.0.12 -d 0.0.0.0/0 --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT 
+# Reglas para el proxy
+iptables -A FORWARD -p tcp -s 0.0.0.0/0 -d 10.0.0.26/32 --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT 
+iptables -A FORWARD -p tcp -s 10.0.0.26 -d 0.0.0.0/0 --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT 
+iptables -A FORWARD -p tcp -s 0.0.0.0/0 -d 10.0.0.26/32 --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT 
+iptables -A FORWARD -p tcp -s 10.0.0.26 -d 0.0.0.0/0 --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT 
+# Reglas para el servidor privado
+iptables -A FORWARD -p udp -s 0.0.0.0/0 -d 10.0.0.39/32 --sport 1024:65535 --dport 51820 -j ACCEPT 
+iptables -A FORWARD -p udp -s 10.0.0.39 -d 0.0.0.0/0 --sport 51820 --dport 1024:65535 -j ACCEPT 
+
+# Reglas por defecto DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
-
-# Reglas para el servidor web público (NAT y FORWARD)
-
-iptables -t nat -A PREROUTING -p tcp --dport 80 -d 10.0.0.100 -i eth0 -j DNAT --to-destination 10.0.0.12
-iptables -t nat -A POSTROUTING -p tcp --sport 80 -s 10.0.0.12 -o eth0 -j SNAT --to-source 10.0.0.100
-
-iptables -A FORWARD -p tcp -s 0.0.0.0/0 -d 10.0.0.12/32 --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT 
-iptables -A FORWARD -p tcp -s 10.0.0.12 -d 0.0.0.0/0 --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT 
-
-# Reglas para el proxy (NAT y FORWARD)
-
-iptables -t nat -A POSTROUTING -p tcp --dport 80 -s 10.0.0.26 -o eth0 -j SNAT --to-source 10.0.0.100
-
-iptables -A FORWARD -p tcp -s 0.0.0.0/0 -d 10.0.0.26/32 --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT 
-iptables -A FORWARD -p tcp -s 10.0.0.26 -d 0.0.0.0/0 --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT 
-
-iptables -t nat -A POSTROUTING -p tcp --dport 443 -s 10.0.0.26 -o eth0 -j SNAT --to-source 10.0.0.100
-
-iptables -A FORWARD -p tcp -s 0.0.0.0/0 -d 10.0.0.26/32 --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT 
-iptables -A FORWARD -p tcp -s 10.0.0.26 -d 0.0.0.0/0 --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT 
-
-# Reglas para el servidor privado (NAT y FORWARD)
-
-iptables -t nat -A PREROUTING -p tcp -d 10.0.0.100 -i eth0 -j DNAT --to-destination 10.0.0.39
-iptables -t nat -A POSTROUTING -p tcp -s 10.0.0.39 -o eth0 -j SNAT --to-source 10.0.0.100
---dport 51820 
---sport 51820 
-
-iptables -A FORWARD -p tcp -s 0.0.0.0/0 -d 10.0.100.39/32 --sport 1024:65535 --dport 51820 -m state --state NEW,ESTABLISHED -j ACCEPT 
-iptables -A FORWARD -p tcp -s 10.0.100.39 -d 0.0.0.0/0 --sport 51820 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT 
