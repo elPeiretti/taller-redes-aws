c62fe68d0e1ded02f95c6c46e678131230844d48
ChortJulio <chortjulio@gmail.com>
2023-02-13 19:12:26 -0300

    Backend1

--- Subnet_Publica/backend1_reglas.sh
+++ Subnet_Publica/backend1_reglas.sh
@@ -8,8 +8,8 @@ iptables -F FORWARD
 
 # Reglas para poder conectarse por ssh si se necesita
 
-iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.4/32 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.4/32 -d 0.0.0.0/0 -p tcp --sport 22 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/0 -d 10.0.0.5/32 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.5/32 -d 0.0.0.0/0 -p tcp --sport 22 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
 
 # Reglas por defecto
 
@@ -18,7 +18,7 @@ iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
 # Reglas para permitir el trafico http y https desde/hacia balanceador de carga
-iptables -A INPUT -s 10.0.0.12/32 -d 10.0.0.4/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.4/32 -d 10.0.0.12/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -s 10.0.0.12/32 -d 10.0.0.4/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -s 10.0.0.4/32 -d 10.0.0.12/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.7/32 -d 10.0.0.5/32 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.5/32 -d 10.0.0.7/32 -p tcp --sport 80 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.7/32 -d 10.0.0.5/32 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -s 10.0.0.5/32 -d 10.0.0.7/32 -p tcp --sport 443 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
