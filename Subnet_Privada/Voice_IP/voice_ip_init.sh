5b4dbf771fa4a136df6590cb36099f7aee650bd0
ChortJulio <chortjulio@gmail.com>
2023-02-14 11:52:22 -0300

    proxy

--- Subnet_Privada/Voice_IP/voice_ip_init.sh
+++ Subnet_Privada/Voice_IP/voice_ip_init.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
 ## instalar y correr asterisk
+
 sudo apt-get update
 sudo apt install asterisk
 sudo systemctl start asterisk
 
-## recargar configuraciones en el PBX funcional
+# cambiar archivos
+
+cd /etc/asterisk
+# Agregar las líneas escritas en extensions.conf
+sudo nano extensions.conf
+# Agregas las líneas escritas en sip.conf
+sudo nano sip.conf
+
+## entrar a la consola de asterisk
+
 sudo asterisk -rvvvv
-sudo sip reload
-sudo dialplan reload
+
+# poner los siguientes comandos
+
+# sip reload
+# dialplan reload
 
 # reiniciar asterisk
+
 sudo systemctl restart asterisk
