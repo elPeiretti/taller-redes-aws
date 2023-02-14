3bf2a3318cf5ec3ca52f435bd29a6fe75f01791d
ChortJulio <chortjulio@gmail.com>
2023-02-14 10:54:34 -0300

    Servidor VPN

--- Subnet_VPN/servidor_vpn_init.sh
+++ Subnet_VPN/servidor_vpn_init.sh
@@ -4,23 +4,30 @@
 sudo apt-get update
 sudo apt install wireguard
 
+# acceder a la carpeta /etc/wireguard
+sudo su
+
 ## generar claves
-sudo cd /etc/wireguard
+cd /etc/wireguard
 #crear clave privada
-sudo wg genkey > tp_private_key
+wg genkey > tp_private_key
 # crear clave publica
-sudo wg pubkey < tp_private_key > tp_public_key
+wg pubkey < tp_private_key > tp_public_key
 
 # crear archivo de configuracion
-sudo nano tr0.conf
-sudo chmod 600 tp_private_key tp_public_key tr0.conf
+nano tr0.conf
+
+exit
+sudo su
+
+chmod 600 tp_private_key tp_public_key tr0.conf
 
 ## levantar wireguard
-sudo systemctl enable --now wg-quick@tr0
+systemctl enable --now wg-quick@tr0
 
 # Descomentar el net.ipv4.ip_forward = 1
-sudo nano /etc/sysctl.conf
+nano /etc/sysctl.conf
 
-sudo sysctl -p
+sysctl -p
 
-sudo sysctl --system
+sysctl --system
